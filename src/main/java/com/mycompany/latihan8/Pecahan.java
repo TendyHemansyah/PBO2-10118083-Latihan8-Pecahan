d700e272b70f34b0f66df09ee3f1e3a8136f649c
Tendy Hermansyah <55569231+TendyHemansyah@users.noreply.github.com>
2019-09-26 11:24:10 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/latihan8/Pecahan.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.latihan8;
+
+/**
+ *
+ * @author Tendy
+ * NAMA     : Tendy Hermansyah
+ * KELAS    : PBO2
+ * NIM      : 10118083
+ * Deskripsi Program : Pecahan
+ */
+public class Pecahan {
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+}
